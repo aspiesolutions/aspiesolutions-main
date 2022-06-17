4e6c4a570d2da35051e63772973cf50e59f388ac
Jordan Morris <jthecybertinkerer@gmail.com>
2022-06-17 18:29:02 -0500

    working on schema now

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,30 +1,38 @@
 // import type { NextApiResponse, NextApiRequest } from "next"
 import "reflect-metadata";
 import { resolvers } from "@generated/type-graphql";
+import fs from "fs";
+import path from "path";
 import { buildSchema, buildTypeDefsAndResolvers } from "type-graphql";
-import { ApolloServer } from "apollo-server-micro";
-import { makeExecutableSchema } from "@graphql-tools/schema";
+// import { ApolloServer } from "apollo-server-micro";
+// import { makeExecutableSchema } from "@graphql-tools/schema";
 import { graphql, printSchema } from "graphql";
-import corsAsync from "cors-async";
+// import corsAsync from "cors-async";
 import { NextApiRequest, NextApiResponse } from "next";
+
+import { PrismaClient } from "@prisma/client";
+
 
 const schema = await buildSchema({
   resolvers,
   validate: false,
 });
-// const schema = makeExecutableSchema({typeDefs,resolvers})
-// const apolloServer = new ApolloServer( {
-//     schema,
-//     cors: false
-// })
+// export the schema file if not in production
+if (process.env.NODE_ENV !== "production") {
+  fs.writeFileSync(
+    path.resolve(process.cwd(), "schema.graphql"),
+    printSchema(schema),
+    { flag: "w" }
+  );
+}
+// this configures this nextjs api route
 export const config = {
   api: {
     bodyParser: true,
   },
 };
-
-// let s = await apolloServer.start()
-// let handler = await apolloServer.createHandler({path:"/api/graphql",})
+// create the prisma ORM client
+const prisma = new PrismaClient()
 export default async function graphqlHandler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -42,17 +50,29 @@ export default async function graphqlHandler(
     return;
   }
   if (req.method == "POST") {
+    console.dir(req.body);
+    // extract query from body if its a string, else extract it from query object on body, else null
+    let query =
+      typeof req.body === "string" ? req.body : req?.body?.query || null;
+    // extract query variables if on body as variables, otherwise null
+    let variables = req?.body?.variables || null;
+    let operationName = req?.body?.operationName || null;
 
     try {
-      let executionResult = await graphql({ schema, source: typeof req.body === "string" ? req.body : req.body?.query });
+      let executionResult = await graphql({
+        schema,
+        source: typeof req.body === "string" ? req.body : req.body?.query,
+        variableValues: variables,
+        contextValue: {prisma},
+        operationName,
+      });
       if (executionResult.errors) {
         res.status(400);
       } else {
         res.status(200);
-        res.setHeader("content-type","application/json");
-        res.send(JSON.stringify(executionResult))
       }
-      res.send(null);
+      res.setHeader("content-type", "application/json");
+      res.send(JSON.stringify(executionResult));
     } catch (executionError) {
       console.error(executionError);
       res.status(500).send(null);
