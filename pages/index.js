ce0666c5292e6f39ef7b615edd104b12e1de3cae
Jordan Morris <jthecybertinkerer@gmail.com>
2022-06-21 07:19:53 -0500

    working on database logic for this page

--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,106 @@
-import Head from 'next/head'
+import Head from "next/head";
 // import Image from 'next/image'
-import mapboxgl from "!mapbox-gl"
-import "mapbox-gl/dist/mapbox-gl.css"
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import mapboxgl from "!mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 // import { Address } from '@universe/address-parser'
-import { useCallback, useEffect, useRef, useState } from 'react'
-import styles from '../styles/Home.module.css'
+import { useCallback, useEffect, useRef, useState } from "react";
+import styles from "../styles/Home.module.css";
+import { gql, useMutation, useQuery } from "urql";
+import { useRouter } from "next/router";
 // import parseAddress from "../lib/parseAddress"
-const defaultMapboxToken = "pk.eyJ1IjoianRoZWN5YmVydGlua2VyZXIiLCJhIjoiY2w0bjRicWFzMWs2eTNpcGd5c2UyYm1tbCJ9.gtMxHjwKheor-JFsyfx19g"
-mapboxgl.accessToken = defaultMapboxToken
-function checkGeoLocation(){
-  console.log("checking for geolocation")
-  if(typeof window === "object" && "navigator" in window) {
-    console.log("window.navigator is present")
-    try {
-      window.navigator.geolocation.getCurrentPosition(console.log,setGeoError)
-    }
-    catch (geoError) {
-      console.error("geolocation is not available")
+const defaultMapboxToken =
+  "pk.eyJ1IjoianRoZWN5YmVydGlua2VyZXIiLCJhIjoiY2w0bjRicWFzMWs2eTNpcGd5c2UyYm1tbCJ9.gtMxHjwKheor-JFsyfx19g";
+mapboxgl.accessToken = defaultMapboxToken;
+
+const addressQuery = gql`
+  query getFirstAddressQuery($address: String!) {
+    findFirstAddress(where: { text: { equals: $address } }) {
+      text
+      access_codes {
+        value
+      }
     }
   }
-}
+  # query getClosestAddressesQuery {
+
+  # }
+`;
+
+const addAddressWithDefaultsMutation = gql`
+  mutation addAddressWithDefaults($text: String!) {
+    createAddress(data: { text: $text }) {
+      text
+    }
+  }
+`;
+
 export default function Home(props) {
   // check for geolocation
+  const router = useRouter();
   const mapboxContainer = useRef(null);
   const map = useRef(null);
-  const [lat,setLat] = useState(0)
-  const [lng,setLng] = useState(0)
-  const [zoom, setZoom] = useState(1)
-  useEffect(()=>{
-    if(map.current) return;
+  const [lat, setLat] = useState(-98.8223185136653);
+  const [lng, setLng] = useState(31.8039734986);
+  const [mapboxAddressResult, setMapboxAddress] = useState(null);
+  const [zoom, setZoom] = useState(5);
+  // const [gqlAddressQueryResult, reexecuteAddressQuery] = useQuery({
+  //   query: addressQuery,
+  //   variables: { address: mapboxAddressResult?.result?.place_name || "" },
+  // });
+  // const [gqlCreateAddressMutationResult,createAddress] = useMutation(addAddressWithDefaultsMutation)
+  useEffect(() => {
+    if (map.current) return;
     map.current = new mapboxgl.Map({
-      container:mapboxContainer?.current,
-      style:"mapbox://styles/mapbox/streets-v11",
-      center:[50,50],
+      container: mapboxContainer?.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lat, lng],
       zoom,
-
-    })
+    });
     map.current.addControl(
       new mapboxgl.GeolocateControl({
         positionOptions: {
-          enableHighAccuracy:true
+          enableHighAccuracy: true,
         },
-        trackUserLocation:true,
-        showUserHeading:true
+        trackUserLocation: true,
+        showUserHeading: true,
       })
-    )
-    map.current.addControl(
-      new MapboxGeocoder({
-        accessToken:mapboxgl.accessToken,
-        mapboxgl:mapboxgl
-      })
-    )
-  })
-  useEffect(()=>{
-    if(!map.current) return;
-    map.current.on('move',()=>{
-      setLng(map.current.getCenter().lng.toFixed(16))
-      setLat(map.current.getCenter().lat.toFixed(16))
-      setZoom(map.current.getZoom())
-    })
-  })
+    );
+    let geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl,
+    });
+    geocoder.on("result", (selected) => {
+      console.log("selected location");
+      console.dir(selected);
+      setMapboxAddress(selected);
+    });
+    map.current.addControl(geocoder);
+  });
+
+  useEffect(() => {
+    if (!map.current) return;
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(16));
+      setLat(map.current.getCenter().lat.toFixed(16));
+      setZoom(map.current.getZoom());
+    });
+  });
+  useEffect(() => {
+    // update the query params when the user types in an address
+    if (!router.isReady || mapboxAddressResult == null) return;
+    // we have to check if the address matches, otherwise an infinite update loop occurs
+    console.log("update query useffect");
+    if (router.query.address == mapboxAddressResult?.result?.place_name) return;
+    router.replace({
+      pathname: router.pathname,
+      query: {
+        ...router.query,
+        address: mapboxAddressResult?.result?.place_name,
+      },
+    });
+  }, [router, mapboxAddressResult]);
   return (
     <div className={styles.container}>
       <Head>
@@ -71,36 +110,84 @@ export default function Home(props) {
       </Head>
 
       <main>
-      <div>{zoom} {lat}, {lng}</div>
-      <div id="mapbox-container" style={{minHeight:"200px"}} ref={mapboxContainer}></div>
+        <div>
+          {zoom} {lat}, {lng}
+        </div>
+        <div
+          id="mapbox-container"
+          style={{ minHeight: "200px" }}
+          ref={mapboxContainer}
+        ></div>
+        <div>current Address {mapboxAddressResult?.result?.place_name}</div>
+        {/* {mapboxAddressResult && gqlAddressQueryResult.fetching
+          ? "loading"
+          : null}
+        {mapboxAddressResult &&
+        gqlAddressQueryResult.fetching === false &&
+        gqlAddressQueryResult.data?.findFirstAddress == null
+          ? "This address does not exist in the databsae"
+          : null}
+        {mapboxAddressResult &&
+        gqlAddressQueryResult.fetching === false &&
+        gqlAddressQueryResult.data?.findFirstAddress?.access_codes?.length == 0
+          ? "No Access Codes for this address"
+          : null} */}
       </main>
-
     </div>
-  )
+  );
 }
-
 
 export async function getServerSideProps(context) {
   // cities and states can be fetched on the server side first
-  const prisma = require("../lib/prisma").default
+  const prisma = require("../lib/prisma").default;
+  const { Address, parse } = require("@universe/address-parser");
   let situs = null;
   let uspsLabel = null;
   let text = null;
+  let exactMatches = null;
   let queryAddress = context?.query?.address || null;
-  if(queryAddress) {
-    const {Address, parse} = require("@universe/address-parser");
-    console.log("address",queryAddress)
-    try {
-      situs = await parse(queryAddress)
-      let address = new Address(situs)
-      let uspsLabel = address.label()
-      let text = address.print()
-    }
-    catch (parseError) {
-      console.error(parseError)
-    }
-    console.log(situs)
+  let newAddress = null
+  if (queryAddress) {
+    // search the database where the text is an exact match
+    situs = parse(queryAddress);
+    uspsLabel = Address.print(situs);
+    console.log(situs);
+    console.log(uspsLabel);
+    // try to find the address by parts
+    exactMatches = await prisma.address.findMany({
+      where: {
+        number: { equals: situs.number },
+        streetName: { equals: situs.streetName },
+        streetType: { equals: situs.streetType },
+        city: { equals: situs.city },
+        state: { equals: situs.state },
+        country: { equals: situs.country },
+      },
+    });
   }
+  if(exactMatches.length == 0) {
+    try {
 
-  return {props:{address:{query:queryAddress,situs,uspsLabel,text}}}
+      newAddress = await prisma.address.create({
+        data:{...situs,
+          text:uspsLabel}
+        })
+        console.log(exactMatches)
+      }
+      catch(newAddressError) {
+        console.error(newAddressError)
+      }
+  }
+  console.log(exactMatches);
+  return {
+    props: {
+      address: {
+        query: queryAddress,
+        situs,
+        uspsLabel,
+        text,
+        matches: { exact: exactMatches },
+      },
+    },
+  };
 }
