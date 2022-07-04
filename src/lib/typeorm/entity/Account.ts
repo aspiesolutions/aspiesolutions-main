3cf711e427c086b0350abcff5aaf79d436369521
Jordan Morris <jthecybertinkerer@gmail.com>
2022-07-04 17:10:01 -0500

    update entity Account to explictly delcare types

--- src/lib/typeorm/entity/Account.ts
+++ src/lib/typeorm/entity/Account.ts
@@ -2,27 +2,29 @@ import "reflect-metadata"
 import {Entity,PrimaryGeneratedColumn,Column,ManyToOne} from "typeorm"
 import type {Relation} from "typeorm"
 import { User } from "./User"
+//We are using babel, types must be explicitly declared
+// in the decorator for the ORM to work correctly
 @Entity()
 export class Account {
     @PrimaryGeneratedColumn()
     id:string
     @ManyToOne(()=>User,(user)=>user.id)
     user:Relation<User>
-    @Column()
+    @Column({type:"text",nullable:false})
     type:string
-    @Column()
+    @Column({type:"text",nullable:false})
     provider:string
-    @Column()
+    @Column({type:"text",nullable:false})
     providerAccountId:string
     @Column({type:"text",nullable:true})
     refresh_token:string | null
-    @Column({nullable:false})
+    @Column({type:"text",nullable:false})
     access_token:string
-    @Column({nullable:false})
+    @Column({type:"int",nullable:false})
     expires_at:number
-    @Column({nullable:true})
+    @Column({type:"text",nullable:true})
     id_token:string
-    @Column({nullable:false})
+    @Column({type:"text",nullable:false})
     token_type:string
     @Column({type:"text"})
     scope:string
