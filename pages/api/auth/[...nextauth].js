6b2fc17d4eec44d2d0bb0a971d90ebddd865b03a
Jordan Morris <jthecybertinkerer@gmail.com>
2022-07-04 12:43:22 -0500

    fix moved import lib to src/lib

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
 
-import { authOptions } from "../../../lib/nextAuth";
+import { authOptions } from "../../../src/lib/nextAuth";
 
 export default NextAuth(authOptions);
