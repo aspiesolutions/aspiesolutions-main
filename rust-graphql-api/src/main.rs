4caa74960bbce90686281eb6ce41e2bbd545d524
Jordan Morris <jthecybertinkerer@gmail.com>
2022-07-19 08:31:02 -0500

    add case to graphql api server to allow routing to be configured via GRAPHQL_HANDLER_PUBLIC_SEGMENT

--- rust-graphql-api/src/main.rs
+++ rust-graphql-api/src/main.rs
@@ -3,6 +3,7 @@ pub mod routes;
 use rocket_db_pools::Database;
 pub use routes::*;
 pub mod lib;
+
 #[launch]
 fn rocket() -> _ {
     let schema = lib_graphql::Schema::new(
@@ -12,7 +13,16 @@ fn rocket() -> _ {
     );
     rocket::build()
     .attach(lib::Db::init()).manage(schema).mount(
-        "/",
+        // check for the existance of an environment variable. if found, mount this route at that path
+        // used for when external routing is also used
+        match std::env::var("GRAPHQL_HANLDER_PUBLIC_SEGMENT") {
+            Ok(s)=>{
+                s
+            }
+            _=>{
+                routes::GRAPHQL_PUBLIC_SEGMENT.to_string()
+            }
+        },
         rocket::routes![
             handle_graphql_post_request,
             handle_graphql_get_request,
--- rust-graphql-api/src/routes.rs
+++ rust-graphql-api/src/routes.rs
@@ -1,17 +1,20 @@
 use lib_graphql::{Schema, Context};
 use rocket::State;
 
-#[rocket::post("/graphql",format="json",data="<body>")]
+pub const GRAPHQL_PUBLIC_SEGMENT:&'static str ="/graphql";
+// we have to mount these paths as '/' and have the runners of this program choose whether to mount thim as '/' or as '/graphql'
+// for instances where we have external routing defined
+#[rocket::post("/",format="json",data="<body>")]
 pub async fn handle_graphql_post_request(schema: &State<Schema>,body: juniper_rocket::GraphQLRequest) -> juniper_rocket::GraphQLResponse  {
     let context = Context {};
     body.execute(&*schema, &context).await
 
 }
-#[rocket::get("/graphql")]
+#[rocket::get("/")]
 pub async fn handle_graphql_get_request<'a>(schema: &State<Schema>) -> String {
     schema.as_schema_language()
 }
-#[rocket::get("/graphql?<query>")]
+#[rocket::get("/?<query>")]
 pub async fn handle_graphql_get_query<'a>(schema: &State<Schema>,query:juniper_rocket::GraphQLRequest)-> juniper_rocket::GraphQLResponse {
     let context = Context {};
     query.execute(&*schema, &context).await
